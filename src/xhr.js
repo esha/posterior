52b203571b5c40887da2729a87baa91beec4ebf2
nbubna <nathan@esha.com>
2014-09-08 11:40:16 -0700

    quick fix

--- src/xhr.js
+++ src/xhr.js
@@ -59,7 +59,7 @@ XHR.promise = function(xhr, cfg) {
     return new Promise(function(resolve, reject) {
         xhr.onload = function() {
             try {
-                XHR.json(xhr);
+                XHR.parse(xhr);
                 XHR.headers(xhr);
                 var status = xhr.status || 200;// file: reports 0, treat as 200
                 (status >= 200 && status < 400 ? resolve : reject)(xhr);
@@ -85,9 +85,9 @@ XHR.promise = function(xhr, cfg) {
     .catch(cfg.retry ? XHR.retry : null);
 };
 
-XHR.json = function(xhr) {
+XHR.parse = function(xhr) {
     try {
-        xhr.responseJson = JSON.parse(xhr.responseText);
+        xhr.value = JSON.parse(xhr.responseText);
     } catch (e) {}
 };
 
@@ -107,7 +107,6 @@ XHR.headers = function(xhr) {
 
 XHR.active = 0;
 XHR.end = function(xhr) {
-    window.console.log('end', this, xhr);
     XHR.active--;
     var handler = xhr.cfg[xhr.status] || global[xhr.status];
     return handler && handler(xhr) || xhr;
