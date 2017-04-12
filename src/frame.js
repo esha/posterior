9c259ec03cfd89fbe9f1143e6bed4a747ffa5bfd
Nathan Bubna <nathan@esha.com>
2017-04-12 15:41:32 -0700

    convenience shortcut for simple Posterior creation

--- src/frame.js
+++ src/frame.js
@@ -3,6 +3,12 @@
     "use strict";
 
 var Posterior = window.Posterior = function(config, name) {
+    if (typeof config === "string") {
+        config = { url: config };
+    }
+    if (typeof name === "function") {
+        config.then = name;
+    }
     return Posterior.api(config, name);
 };
 <%= content %>
