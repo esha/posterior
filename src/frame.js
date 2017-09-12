8d5c8b53476c62ea86629031d0e889af1e1579c0
Nathan Bubna <nathan@esha.com>
2017-09-12 15:51:10 -0700

    make window dependency syntactically optional for now

--- src/frame.js
+++ src/frame.js
@@ -1,17 +1,20 @@
 <%= banner %>
-(function(D, store) {
+(function(W) {
     "use strict";
 
-var Posterior = window.Posterior = function(config, name) {
-    if (typeof config === "string") {
-        config = { url: config };
-    }
-    if (typeof name === "function") {
-        config.then = name;
-    }
-    return Posterior.api(config, name);
-};
+var store = W.store || function(){},
+    D = W.document,
+    Posterior = W.Posterior = function(config, name) {
+        if (typeof config === "string") {
+            config = { url: config };
+        }
+        if (typeof name === "function") {
+            config.then = name;
+        }
+        return Posterior.api(config, name);
+    };
+
 <%= content %>
 Posterior.version = "<%= pkg.version %>";
 
-})(window.document, window.store || function(){});
+})(window || this);
